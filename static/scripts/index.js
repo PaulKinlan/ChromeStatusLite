c8d950ec4a15d3d5d4e4e722506744ebb4de9f98
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-07 11:41:19 +0100

    Updating

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -35,6 +35,31 @@ const updateUI = async (version) => {
   ${renderEnabled(enabled, version)}
   ${renderOriginTrials(originTrials, version)}
   ${renderFlaggedFeatures(flaggedFeatures, version)}
+  <h2>Deprecations and Removals</h2>
+  <h3 id="deprecation-policy">Deprecation policy</h2>
+    <p>To keep the platform healthy, we sometimes remove APIs from the Web Platform which have run their course. There
+        can be many reasons why we would remove an API, such as:</p>
+    <ul>
+        <li>They are superseded by newer APIs.</li>
+        <li>They are updated to reflect changes to specifications to bring alignment and consistency with other
+            browsers.</li>
+        <li>They are early experiments that never came to fruition in other browsers and thus can increase the burden of
+            support for web developers.</li>
+    </ul>
+    <p>Some of these changes will have an effect on a very small number of sites. To mitigate issues ahead of time, we
+        try to give developers advanced notice so they can make the required changes to keep their sites running.</p>
+    <p>Chrome currently has a <a href="http://www.chromium.org/blink#TOC-Launch-Process:-Deprecation">process for
+            deprecations and removals of API's</a>, essentially:</p>
+    <ul>
+        <li>Announce on the <a href="https://groups.google.com/a/chromium.org/forum/#!forum/blink-dev">blink-dev</a>
+            mailing list.</li>
+        <li>Set warnings and give time scales in the Chrome DevTools Console when usage is detected on the page.</li>
+        <li>Wait, monitor, and then remove the feature as usage drops.</li>
+    </ul>
+    <p>You can find a list of all deprecated features on chromestatus.com using the <a
+            href="https://www.chromestatus.com/features#deprecated">deprecated filter</a> and removed features by
+        applying the <a href="https://www.chromestatus.com/features#removed">removed filter</a>. We will also try to
+        summarize some of the changes, reasoning, and migration paths in these posts.</p>
   ${renderDeprecatedFeatures(deprecated, version)}
   ${renderRemovedFeatures(removed, version)}
   `, outputEl);
@@ -110,53 +135,29 @@ const renderFlaggedFeatures = (flagged, version) => html`
 )}`;
 
 const renderDeprecatedFeatures = (deprecated, version) => html`
-    <h2 id="deprecated">Deprecated features in ${version}</h2>
-    <h3 id="deprecation-policy">Deprecation policy</h2>
-    <p>To keep the platform healthy, we sometimes remove APIs from the Web Platform which have run their course. There
-        can be many reasons why we would remove an API, such as:</p>
-    <ul>
-        <li>They are superseded by newer APIs.</li>
-        <li>They are updated to reflect changes to specifications to bring alignment and consistency with other
-            browsers.</li>
-        <li>They are early experiments that never came to fruition in other browsers and thus can increase the burden of
-            support for web developers.</li>
-    </ul>
-    <p>Some of these changes will have an effect on a very small number of sites. To mitigate issues ahead of time, we
-        try to give developers advanced notice so they can make the required changes to keep their sites running.</p>
-    <p>Chrome currently has a <a href="http://www.chromium.org/blink#TOC-Launch-Process:-Deprecation">process for
-            deprecations and removals of API's</a>, essentially:</p>
-    <ul>
-        <li>Announce on the <a href="https://groups.google.com/a/chromium.org/forum/#!forum/blink-dev">blink-dev</a>
-            mailing list.</li>
-        <li>Set warnings and give time scales in the Chrome DevTools Console when usage is detected on the page.</li>
-        <li>Wait, monitor, and then remove the feature as usage drops.</li>
-    </ul>
-    <p>You can find a list of all deprecated features on chromestatus.com using the <a
-            href="https://www.chromestatus.com/features#deprecated">deprecated filter</a> and removed features by
-        applying the <a href="https://www.chromestatus.com/features#removed">removed filter</a>. We will also try to
-        summarize some of the changes, reasoning, and migration paths in these posts.</p>
+    <h3 id="deprecated">Deprecated features in ${version}</h3>
     <p>This realease of Chrome had ${deprecated.length} features deprecated.</p>
     ${deprecated.map(item =>
-  html`<h3>${item.name}</h3>
+  html`<h4>${item.name}</h4>
       <p>${item.summary} <a href=${item.launch_bug_url}>#</a></p>
       ${('motivation' in item) ? html`<p>${item.creator} created this feature because: <blockquote>${item.motivation}</blockquote></p>` : html``}
       <p>This feature was initially proposed in <a href=${item.initial_public_proposal_url}>${item.initial_public_proposal_url}</a></p>
       <p>This feature specified in "<a href=${item.standards.spec}>${item.standards.status.text}</a>"
-      <h4>Resources</h4>
+      <h5>Resources</h5>
       ${('docs' in item.resources) ? html`<p>Docs: ${item.resources.docs.map(resource => html`<a href=${resource}>${resource}</a>`)}</p>` : html`No linked docs`}</p>
       ${('samples' in item.resources) ? html`<p>Samples: ${item.resources.samples.map(resource => html`<a href=${resource}>${resource}</a>`)}</p>` : html`No linked samples`}</p>`
 )}`;
 
 const renderRemovedFeatures = (removed, version) => html`
-    <h2 id="removed">Removed features in ${version}</h2>
+    <h3 id="removed">Removed features in ${version}</h3>
     <p>This realease of Chrome had ${removed.length} features removed.</p>
     ${removed.map(item =>
-  html`<h3>${item.name}</h3>
+  html`<h4>${item.name}</h4>
       <p>${item.summary} <a href=${item.launch_bug_url}>#</a></p>
       ${('motivation' in item) ? html`<p>${item.creator} created this feature because: <blockquote>${item.motivation}</blockquote></p>` : html``}
       <p>This feature was initially proposed in <a href=${item.initial_public_proposal_url}>${item.initial_public_proposal_url}</a></p>
       <p>This feature specified in "<a href=${item.standards.spec}>${item.standards.status.text}</a>"
-      <h4>Resources</h4>
+      <h5>Resources</h5>
       ${('docs' in item.resources) ? html`<p>Docs: ${item.resources.docs.map(resource => html`<a href=${resource}>${resource}</a>`)}</p>` : html`No linked docs`}</p>
       ${('samples' in item.resources) ? html`<p>Samples: ${item.resources.samples.map(resource => html`<a href=${resource}>${resource}</a>`)}</p>` : html`No linked samples`}</p>`
 )}`;
