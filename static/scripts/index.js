44f3313254274569fb943bd7a94120fc9977d6e2
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-07 10:41:09 +0100

    Testing version url

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,5 +1,37 @@
 import { html, render } from 'https://unpkg.com/lit-html?module';
 
+const updateUI = (version) => {
+  if (version == undefined) version = 100;
+
+  const versionResponse = await fetch(`/api/features?version=${version}`);
+  const versionData = await versionResponse.json();
+
+  const featuresByType = versionData.features_by_type;
+
+  const enabled = featuresByType["Enabled by default"];
+  const originTrials = featuresByType["Origin trial"];
+  const flaggedFeatures = featuresByType["In developer trial (Behind a flag)"];
+  const removed = featuresByType["Removed"];
+  const deprecated = featuresByType["Deprecated"];
+
+  render(html`
+  <h1>Chrome ${version}</h1>
+  <a href="#enabled">Enabled</a> | <a href="#origin-trial">Origin Trial</a> | <a href="#flagged">Flagged</a> | <a href="#deprecated">Deprecated</a> | <a href="#removed">Removed</a>
+  ${renderEnabled(enabled, version)}
+  ${renderOriginTrials(originTrials, version)}
+  ${renderFlaggedFeatures(flaggedFeatures, version)}
+  ${renderDeprecatedFeatures(deprecated, version)}
+  ${renderRemovedFeatures(removed, version)}
+  `, outputEl);
+}
+
+window.addEventListener('popstate', (event) => {
+  const url = new URL(location);
+  const version = url.searchParams.get("version");
+
+  updateUI(version)
+});
+
 onload = () => {
   const versionEl = document.getElementById("version");
   const outputEl = document.getElementById("output");
@@ -7,31 +39,17 @@ onload = () => {
 
   render(html`${versions.map((item) => html`<option value=${item}>${item}</option>`)}`, versionEl);
 
+  const url = new URL(location);
+  const loadedVersion = url.searchParams.get("version");
+
+  updateUI(loadedVersion);
+
   versionEl.onchange = async (e) => {
-    const version = e.target.value;
-    if (version == undefined) return;
+    const changedVersion = e.target.value;
 
+    navigator.history.pushState({}, undefined, `/?version=${changedVersion}`);
 
-    const versionResponse = await fetch(`/api/features?version=${version}`);
-    const versionData = await versionResponse.json();
-
-    const featuresByType = versionData.features_by_type;
-
-    const enabled = featuresByType["Enabled by default"];
-    const originTrials = featuresByType["Origin trial"];
-    const flaggedFeatures = featuresByType["In developer trial (Behind a flag)"];
-    const removed = featuresByType["Removed"];
-    const deprecated = featuresByType["Deprecated"];
-
-    render(html`
-    <h1>Chrome ${version}</h1>
-    <a href="#enabled">Enabled</a> | <a href="#origin-trial">Origin Trial</a> | <a href="#flagged">Flagged</a> | <a href="#deprecated">Deprecated</a> | <a href="#removed">Removed</a>
-    ${renderEnabled(enabled, version)}
-    ${renderOriginTrials(originTrials, version)}
-    ${renderFlaggedFeatures(flaggedFeatures, version)}
-    ${renderDeprecatedFeatures(deprecated, version)}
-    ${renderRemovedFeatures(removed, version)}
-    `, outputEl);
+    updateUI(version);
   };
 };
 
