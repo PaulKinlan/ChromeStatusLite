4251d4535a5e0a0d512141e9c71a47cd4607da1c
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-13 14:21:51 +0100

    fixing ui

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,17 +1,4 @@
 onload = async () => {
-  const url = new URL(location);
-  let loadedVersion = url.searchParams.get("version");
-
-  const versionEl = document.getElementById("version");
-  const versions = await getVersions();
-
-  if (loadedVersion == null) {
-    loadedVersion = versions[0]; // default to the latest
-    history.pushState({}, undefined, `/?version=${loadedVersion}`);
-  }
-
-  
-
   versionEl.onchange = async (event) => {
     event.form.submit();
   };
