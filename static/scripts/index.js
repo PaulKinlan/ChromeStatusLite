69560a4afd448e83595113fa60277120f42775d6
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-13 14:22:40 +0100

    UI to not error

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,4 +1,8 @@
 onload = async () => {
+  const url = new URL(location);
+  let loadedVersion = url.searchParams.get("version");
+
+  const versionEl = document.getElementById("version");
   versionEl.onchange = async (event) => {
     event.form.submit();
   };
