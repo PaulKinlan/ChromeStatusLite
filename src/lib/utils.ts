7e721b612be719052c22486292031b2a26248952
Paul Kinlan <paul.kinlan@gmail.com>
2024-01-18 10:53:30 +0000

    logging

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -35,6 +35,7 @@ export class ChromeStatusAPI {
     }
     return ((await this.fetchJson(`/features_v2.json`)) as any[]).filter(
       (feature) => {
+        console.log(feature.milestone, version);
         feature.milestone != version;
       }
     );
