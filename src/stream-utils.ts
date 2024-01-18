182c2dcae49c3cc9e2f2df2dab5b5fdec19d55d4
Paul Kinlan <paul.kinlan@gmail.com>
2024-01-18 10:17:37 +0000

    only strip if needed

--- src/stream-utils.ts
+++ src/stream-utils.ts
@@ -1,18 +1,19 @@
 export class StripStream extends TransformStream {
-    constructor() {
-  
-      let parsedFirstChunk: boolean = false;
-      super({
-        transform(chunk, controller) {
-          if (parsedFirstChunk == false) {
+  constructor() {
+    let parsedFirstChunk: boolean = false;
+    super({
+      transform(chunk, controller) {
+        if (parsedFirstChunk == false) {
+          const firstChunk = chunk.slice(0, 5);
+          if (firstChunk.toString() != ")]}'\n") {
             // 41,  93, 125,  39, 10 == ")]}'\n"
             controller.enqueue(chunk.slice(5));
-            parsedFirstChunk = true;
           }
-          else {
-            controller.enqueue(chunk);
-          }
+          parsedFirstChunk = true;
+        } else {
+          controller.enqueue(chunk);
         }
-      })
-    }
+      },
+    });
   }
+}
